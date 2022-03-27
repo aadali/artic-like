bced57842a0b10628dab0c504d7190740c071b9e
aadali <aaaliyin@gmail.com>
2022-03-28 00:24:33 +0800

    change it for new per-base depth of samtools

--- scripts/plot.py
+++ scripts/plot.py
@@ -10,19 +10,6 @@ import pandas as pd
 # fig_path = "test001.coverage.jpg"
 
 
-def coverage_ratio(df, depth):
-    """
-    calculate coverage percentage depend on the depth
-    :param df:
-    :param depth:
-    :return:
-    """
-    sub_df = df.query("depth >= @depth")
-    sub_df['count'] = df['end'] - df['start']
-    length = df['end'].tolist()[-1]
-    return sum(sub_df['count']) / length
-
-
 def xaxis_major_formater(x, pos=None):
     if x % 1000 == 0:
         return f"{int(x / 1000)}k"
@@ -30,13 +17,13 @@ def xaxis_major_formater(x, pos=None):
         return f"{x / 1000:.1f}k"
 
 
-def plot_coverage(per_base_dp, out_dir):
-    df = pd.read_csv(per_base_dp, compression="gzip", sep="\t", header=None)
-    df.columns = ['contig', 'start', 'end', 'depth']
+def plot_coverage(per_base_dp, out_dir, sample_name):
+    df = pd.read_csv(per_base_dp, sep="\t", header=None)
+    df.columns = ['contig', 'position', 'depth']
     grouped = df.groupby("contig")
     for group in grouped.groups:
         sub_df = grouped.get_group(group)
-        length = df['end'].tolist()[-1]
+        length = sub_df.shape[0]
 
         # the different length contig has different bin_width to set ticker
         bins = length // 1000
@@ -51,18 +38,15 @@ def plot_coverage(per_base_dp, out_dir):
 
         title = []
         for i in [1, 10, 100, 1000]:
-            cov = coverage_ratio(sub_df, i)
+            cov = sub_df.query("depth >= @i").shape[0] / length
             title.append(f">={i}X: {cov:.2%}")
 
-        plot_title = f"{group}\n{title[0]:^20}{title[1]:^20}\n{title[2]:^20}{title[3]:^20}"
+        plot_title = f"{sample_name}{group}\n{title[0]:^20}{title[1]:^20}\n{title[2]:^20}{title[3]:^20}"
         fig, ax = plt.subplots(figsize=(12, 6))
-        positions = []
-        depth = []
-        for row in sub_df.itertuples(index=False):
-            positions.extend(list(range(row.start, row.end)))
-            depth.extend((row.end - row.start) * [row.depth])
+        positions = sub_df['position']
+        depth = sub_df['depth']
 
-        depth2 = [dp if dp < 3000 else 3000 for dp in depth]
+        depth2 = [dp if dp < 1200 else 1200 for dp in depth]
         ax.bar(positions, depth2, width=1)
         ax.set_xlabel('Position')
         ax.set_ylabel("Depth")
@@ -74,13 +58,14 @@ def plot_coverage(per_base_dp, out_dir):
         ax.spines['right'].set_visible(False)
         ax.spines['top'].set_visible(False)
         ax.tick_params(axis="x", labelsize=8)
-        plt.savefig(path.join(out_dir, f"{group}.coverage.pdf"))
+        plt.savefig(path.join(out_dir, f"{sample_name}.coverage.pdf"))
 
 
 if __name__ == '__main__':
-    usage = f"usage: {path.basename(__file__)} <per_base_dp.bed> <out_fig_path>"
-    if len(argv) != 3:
+    usage = f"usage: {path.basename(__file__)} <per_base_dp.bed> <out_fig_path> <sample_name>"
+    if len(argv) != 4:
         raise Exception(usage + "\n")
     per_base_dp = argv[1]
     out_dir = argv[2]
-    plot_coverage(per_base_dp, out_dir)
+    sample = argv[3]
+    plot_coverage(per_base_dp, out_dir, sample)
